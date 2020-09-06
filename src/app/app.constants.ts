b5c4f5e9545dded9427a60a303ccf7ae7141ed65
Aeon Tanvir <aeontanvir@gmail.com>
2020-09-06 14:39:13 +0600

    Settings: App constant add

---
+++ src/app/app.constants.ts
@@ -0,0 +1,17 @@
+export const DEBUG_INFO_ENABLED: boolean = true;
+
+export const APP_URL = `//${window.location.hostname}/`;
+export const SERVER_API_URL = `//${window.location.hostname}:${getBackendPort(window.location.port)}/api/`;
+function getBackendPort(port = '') {
+  const portLocal = 9395;
+  const portMap = {
+    80: 9395,
+    8002: 9395,
+    '': 9395
+  };
+  return portMap[port] ? portMap[port] : portLocal;
+}
+
+export const LOCAL_STORAGE_TOKEN = 'simpleAngularAdminAccessToken';
+export const LOCAL_STORAGE_USER = 'simpleAngularAdminUser';
+export const LOCAL_STORAGE_REDIRECT = 'simpleAngularAdminRedirectUrl';
